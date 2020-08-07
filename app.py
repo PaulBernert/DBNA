0f7758d6fa615afe7a59b099a070a3c7216911f9
Paul Bernert <pwberner@asu.edu>
2020-08-06 18:08:48 -0700

    non-descriptive comment here

--- app.py
+++ app.py
@@ -14,5 +14,41 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/analysis")
+def analysis():
+    return render_template("index.html")
+
+@app.route("/clustering")
+def clustering():
+    return render_template("index.html")
+
+@app.route("/methodology/ease-of-doing-business")
+def methodology_eodb():
+    return render_template("index.html")
+
+@app.route("/methodology/starting-a-business")
+def methodology_sab():
+    return render_template("methodology_sab.html")
+
+@app.route("/methodology/employing-workers")
+def methodology_ew():
+    return render_template("index.html")
+
+@app.route("/methodology/getting-electricity")
+def methodology_ge():
+    return render_template("index.html")
+
+@app.route("/methodology/paying-taxes")
+def methodology_pt():
+    return render_template("index.html")
+
+@app.route("/methodology/land-space-use")
+def methodology_lsu():
+    return render_template("index.html")
+
+@app.route("/methodology/resolving-insolvency")
+def methodology_re():
+    return render_template("index.html")
+
 if __name__ == "__main__":
     app.run()
